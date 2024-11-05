d40e026b188e2a4f0aa963494ed7f08bb88ea354
shrutimajale <131251457+shrutimajale@users.noreply.github.com>
2024-11-05 11:57:37 +0530

    Add files via upload

---
+++ build/precache-manifest.5f99bab418ea1da399dba8f3d6e7fe34.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c001f54d1957b6549f211ccfb214bf17",
+    "url": "/BankApp/index.html"
+  },
+  {
+    "revision": "ffb1d4529183986e60f3",
+    "url": "/BankApp/static/css/2.698bb075.chunk.css"
+  },
+  {
+    "revision": "53699e11097df415e015",
+    "url": "/BankApp/static/css/main.0766f131.chunk.css"
+  },
+  {
+    "revision": "ffb1d4529183986e60f3",
+    "url": "/BankApp/static/js/2.89a6849d.chunk.js"
+  },
+  {
+    "revision": "3232a3baa9a05f3e2b89",
+    "url": "/BankApp/static/js/3.9973b74a.chunk.js"
+  },
+  {
+    "revision": "53699e11097df415e015",
+    "url": "/BankApp/static/js/main.4c079caf.chunk.js"
+  },
+  {
+    "revision": "4e6bd21e50b388b45ed5",
+    "url": "/BankApp/static/js/runtime~main.3a8195cb.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/BankApp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/BankApp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/BankApp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/BankApp/static/media/slick.f97e3bbf.svg"
+  }
+]);
